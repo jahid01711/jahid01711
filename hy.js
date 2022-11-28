7169aa067aa2f4bef2d08e2de0cb4eb58a522f82
MD Jahidul Islam <119214763+jahid01711@users.noreply.github.com>
2022-11-28 15:05:47 +0600

    hello

---
+++ hy.js
@@ -0,0 +1,21 @@
+adding few automation codes 
+
+var fs = require('fs');
+
+calculate = () => {
+    fs.readFile('data.txt', 'utf8', (err, data) => {
+        if (err) {
+            throw new Error(err)
+        }
+
+        const arr = data.split('\r\n');
+        const result = arr
+            .filter(e => e)
+            .map(parseFloat)
+            .reduce((curr, next) => curr + next);
+        console.log('RESULT: ', result);
+    });
+}
+
+// the hello world program
+console.log('Hello World');
